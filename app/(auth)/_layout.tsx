13e1c6320b455500ab4e0f3987f638e72a8455a5
gamzenurtukel <gamzenurtukel@gmail.com>
2024-11-24 12:55:10 +0300

    AuthLayout created

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,38 @@
+// import { useAppSelector } from "@/redux/app/hooks";
+// import { selectIsAuthenticated } from "@/redux/reducers/auth-reducer";
+import { Redirect, Stack } from "expo-router";
+
+const AuthLayout = () => {
+  //   const isAuthenticatedd = useAppSelector(selectIsAuthenticated);
+  //   if (isAuthenticatedd) return <Redirect href="/home" />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="sign-in"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="sign-up"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="forgot-password"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="reset-password"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default AuthLayout;
