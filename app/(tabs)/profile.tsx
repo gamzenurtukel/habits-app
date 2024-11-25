ceb9cba06133b1e1f8d32672b0514f3bcce7f3f1
gamzenurtukel <gamzenurtukel@gmail.com>
2024-11-25 19:30:18 +0300

    fixed

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,8 @@
 import {
+    Image,
     StyleSheet,
     Text,
+    TouchableOpacity,
     View,
 } from "react-native";
 import { Link, Stack } from "expo-router";
@@ -11,6 +13,19 @@ export default function ProfileScreen() {
         <SafeAreaView style={styles.container}>
             <Stack.Screen options={{ headerShown: false }} />
             <View style={styles.container}>
+                <View style={styles.card}>
+                    <View style={styles.cardProfile}>
+                        <Image
+                            source={{ uri: "https://via.placeholder.com/150" }}
+                            style={styles.reactLogo}
+                        />
+                        <Text>John Doe</Text>
+                    </View>
+                    <TouchableOpacity >
+                      
+                    </TouchableOpacity>
+
+                </View>
 
             </View>
         </SafeAreaView>
@@ -22,6 +37,33 @@ const styles = StyleSheet.create({
         backgroundColor: "#E8F5E9",
         // flex: 1,
         height: "100%",
+        // padding: 10,
+    },
+    reactLogo: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    },
+    card: {
+        backgroundColor: "#E8F5E9",
+        padding: 10,
+        borderRadius: 10,
+        margin: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    cardProfile: {
+        flexDirection: "row",
+        alignItems: "center",
+        // justifyContent: "space-between",
+        gap: 10,
+
     },
 
 
