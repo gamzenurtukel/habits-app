9df383717bc5586765b7006123e931ef0fe6c880
gamzenurtukel <gamzenurtukel@gmail.com>
2024-11-26 18:55:42 +0300

    authSlice created

---
+++ redux/reducers/auth-reducer.ts
@@ -0,0 +1,66 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { login, logout } from "../services/auth";
+import { RootState } from "../app/store";
+
+const initialState: any = {
+  user: "",
+  token: "",
+  isAuthenticated: true,
+  onBoarding: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth(state, action) {
+      state.isAuthenticated = action.payload;
+    },
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    resetAuth(state) {
+      authSessionClear(state);
+    },
+    setOnboarding(state, action: PayloadAction<boolean>) {
+      state.onBoarding = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addMatcher(login.matchFulfilled, (state, action) => {
+      console.log({ payload: action.payload });
+    });
+    builder.addMatcher(logout.matchFulfilled, (state, action) => {
+      state.user = "";
+      state.token = "";
+      state.isAuthenticated = false;
+      state.turmobToken = false;
+    });
+  },
+});
+
+function authSessionClear(state: any) {
+  state.user = "";
+  state.token = "";
+  state.isAuthenticated = false;
+}
+
+export const { setAuth, setToken, setUser, resetAuth, setOnboarding } =
+  authSlice.actions;
+
+export const selectUserCredentials = (state: RootState) => state.auth;
+
+export const selectToken = (state: RootState) => state.auth.token;
+
+export const selectCurrentUser = (state: RootState) => state.auth.user;
+
+export const selectIsAuthenticated = (state: RootState) =>
+  state.auth.isAuthenticated;
+
+export const selectOnBoarding = (state: RootState) => state.auth.onBoarding;
+
+export default authSlice.reducer;
