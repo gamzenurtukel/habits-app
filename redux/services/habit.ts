9802e963f713739c3abcb8efba537b6832bdd70f
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-01 21:57:44 +0300

    habitGetById,updateHabit,deleteHabit and updateActionHabit created

--- redux/services/habit.ts
+++ redux/services/habit.ts
@@ -1,36 +1,29 @@
+import { IServerResponse } from "@/types/server";
 import { api } from "./api";
+import {
+  IHabit,
+  IHabitActionListData,
+  IHabitCreate,
+  IHabitUpdate,
+  IHabitUpdateAction,
+} from "@/types/habit";
 
 const habitApi = api.injectEndpoints({
   endpoints: (build) => ({
-    createHabit: build.mutation<
-      any,
-      {
-        name: string;
-        description: string;
-        isReminder: boolean;
-        details: {
-          color: string;
-          icon: string;
-          periodType: number;
-          periodCount: number;
-          startTime: string | null;
-          endTime: string | null;
-        };
-      }
-    >({
+    createHabit: build.mutation<IServerResponse<any>, IHabitCreate>({
       query: (body) => ({
         method: "POST",
         url: "/Habit/Api/Habit/Create",
         body: body,
       }),
     }),
-    habitGetList: build.query<any, void>({
+    habitGetList: build.query<IServerResponse<IHabit[]>, void>({
       query: () => ({
         method: "GET",
         url: "/Habit/Api/Habit/GetList?Size=100&PageNumber=0&IsFailed=false",
       }),
     }),
-    habitActionList: build.query<any, any>({
+    habitActionList: build.query<IServerResponse<IHabitActionListData>, any>({
       query: (date) => ({
         method: "GET",
         url: "/Habit/Api/HabitAction/ActionGetList",
@@ -43,8 +36,41 @@ const habitApi = api.injectEndpoints({
         // }),
       }),
     }),
+    habitGetById: build.query<IServerResponse<IHabit>, string>({
+      query: (id) => ({
+        method: "GET",
+        url: `/Habit/Api/Habit/${id}`,
+      }),
+    }),
+    updateHabit: build.mutation<IServerResponse<any>, IHabitUpdate>({
+      query: (body) => ({
+        method: "PUT",
+        url: `/Habit/Api/Habit/Update`,
+        body: body,
+      }),
+    }),
+    deleteHabit: build.mutation<
+      IServerResponse<any>,
+      {
+        id: string;
+      }
+    >({
+      query: (body) => ({
+        method: "DELETE",
+        url: `/Habit/Api/Habit/Delete`,
+        body: body,
+      }),
+    }),
+    updateActionHabit: build.mutation<IServerResponse<any>, IHabitUpdateAction>(
+      {
+        query: (body) => ({
+          method: "PUT",
+          url: "/Habit/Api/HabitAction/UpdateAction",
+          body: body,
+        }),
+      }
+    ),
   }),
-
   overrideExisting: true,
 });
 
@@ -52,7 +78,19 @@ export const {
   useCreateHabitMutation,
   useHabitGetListQuery,
   useHabitActionListQuery,
+  useHabitGetByIdQuery,
+  useUpdateHabitMutation,
+  useDeleteHabitMutation,
+  useUpdateActionHabitMutation,
 } = habitApi;
 export const {
-  endpoints: { createHabit, habitGetList, habitActionList },
+  endpoints: {
+    createHabit,
+    habitGetList,
+    habitActionList,
+    habitGetById,
+    updateHabit,
+    deleteHabit,
+    updateActionHabit,
+  },
 } = habitApi;
