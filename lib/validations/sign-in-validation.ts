86cd9cb6ccb1c2e5d14ef2644a855bf194187c31
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-15 02:17:00 +0300

    fixed

--- lib/validations/sign-in-validation.ts
+++ lib/validations/sign-in-validation.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const signInDataSchema = z.object({
-  email: z.string().email("Geçerli bir e-posta adresi girin."),
+  email: z.string().email("email_invalid"),
   password: z
     .string()
-    .min(6, "Şifre en az 6 karakter olmalıdır.")
-    .max(20, "Şifre en fazla 20 karakter olabilir."),
+    .min(6, "password_min_length_6")
+    .max(20, "password_max_length_20"),
   rememberMe: z.boolean(),
 });
 
