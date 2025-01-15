ac9d1698a2e24ac1bca7a0fd81257e2653cb3edb
Furkan Güleç <glcfrkn@outlook.com>
2025-01-15 17:29:00 +0300

    Adapty installed

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,11 @@ import { selectIsAuthenticated } from "@/redux/reducers/auth-reducer";
 import { Link, Redirect } from "expo-router";
 import React from "react";
 
+// TODO Bunları daha sonra kaldırıp başka yere taşıyabiliriz. 
+import { adapty } from 'react-native-adapty';
+adapty.activate('public_live_4Hlw9dpe.NMnoQYBjNTLQofLN5fDy');
+
+
 const StartScreen = () => {
   const isAuthenticated = useAppSelector(selectIsAuthenticated);
 
