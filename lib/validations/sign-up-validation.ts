5f090e756bcca2ff48a15890dad3f7cee9fd8936
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-18 01:00:11 +0300

    validation messages changed

--- lib/validations/sign-up-validation.ts
+++ lib/validations/sign-up-validation.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const signUpDataSchema = z.object({
-  name: z.string().min(3, "Ad en az 3 karakter olmalıdır."),
-  surname: z.string().min(3, "Soyad en az 3 karakter olmalıdır."),
-  email: z.string().email("Geçerli bir e-posta adresi girin."),
+  name: z.string().min(3, "name_min_3_characters"),
+  surname: z.string().min(3, "surname_min_3_characters"),
+  email: z.string().email("email_invalid"),
   password: z
     .string()
-    .min(6, "Şifre en az 6 karakter olmalıdır.")
-    .max(20, "Şifre en fazla 20 karakter olabilir."),
+    .min(6, "password_min_length_6")
+    .max(20, "password_max_length_20"),
 });
 
 export type SignUpFormValues = z.infer<typeof signUpDataSchema>;
