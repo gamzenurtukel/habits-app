51fae0ceb2b047fdceb2a93f17130f850772c5dc
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-21 00:40:37 +0300

    fixed

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { Link, Redirect } from "expo-router";
 import React from "react";
 import { LogLevel, OneSignal } from "react-native-onesignal";
 import { useEffect } from "react";
+import i18nextConfig from "@/i18n/i18nextConfig";
 
 // TODO Bunları daha sonra kaldırıp başka yere taşıyabiliriz.
 import { adapty } from "react-native-adapty";
@@ -11,46 +12,30 @@ adapty.activate("public_live_4Hlw9dpe.NMnoQYBjNTLQofLN5fDy");
 
 const StartScreen = () => {
   const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const i18n = i18nextConfig;
+
+  console.log("language", i18n.language);
 
   useEffect(() => {
-    // // OneSignal App ID'nizi burada başlatın
-    // OneSignal.setAppId("YOUR_ONESIGNAL_APP_ID");
-
-    // // Bildirim açılma olaylarını dinleyin
-    // OneSignal.setNotificationOpenedHandler((notification) => {
-    //   console.log("Notification opened:", notification);
-    // });
-
-    // // Uygulama çalışırken bildirimin görünümünü ayarlayın (opsiyonel)
-    // OneSignal.setNotificationWillShowInForegroundHandler(
-    //   (notificationEvent) => {
-    //     const notification = notificationEvent.getNotification();
-    //     console.log("Notification received in foreground:", notification);
-
-    //     // Bildirimi göstermek için notificationEvent.complete() çağrılır
-    //     notificationEvent.complete(notification);
-    //   }
-    // );
-
-    // // Bildirim izni isteme
-    // OneSignal.promptForPushNotificationsWithUserResponse((response) => {
-    //   console.log("User granted notification permission:", response);
-    // });
-    // Remove this method to stop OneSignal Debugging
-
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
     // OneSignal Initialization
-    OneSignal.initialize("ONESIGNAL_APP_ID");
+    OneSignal.initialize("dff0b398-b1ee-4ddd-9b48-e23b3bb4de1d");
 
     // requestPermission will show the native iOS or Android notification permission prompt.
     // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
     OneSignal.Notifications.requestPermission(true);
+    OneSignal.User.setLanguage(i18n.language || "tr");
 
     // Method for listening for notification clicks
     OneSignal.Notifications.addEventListener("click", (event) => {
       console.log("OneSignal: notification clicked:", event);
     });
+
+    OneSignal.InAppMessages.addTrigger("key", "value");
+
+    // location permission
+    OneSignal.Location.requestPermission();
   }, []);
 
   if (!isAuthenticated) return <Redirect href="/(auth)/sign-in" />;
