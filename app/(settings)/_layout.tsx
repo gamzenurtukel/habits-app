a43cecd7630f695516d5da04f0f7bccbeb867377
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-23 22:35:40 +0300

    SettingsLayout created

---
+++ app/(settings)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Redirect, Stack } from "expo-router";
+
+const SettingsLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="settings"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="web-view"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default SettingsLayout;
