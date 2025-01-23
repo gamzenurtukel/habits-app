ea2896f22f7a508bbdd662212c84e7cda4e105bf
gamzenurtukel <gamzenurtukel@gmail.com>
2025-01-23 04:42:40 +0300

    IAppleWithSignIn and IGoogleWithSignIn added

--- types/auth.ts
+++ types/auth.ts
@@ -15,3 +15,14 @@ export interface IConfirmEmail {
   token: string;
   email: string;
 }
+export interface IAppleWithSignIn {
+  identityToken: string;
+  authorizationCode: string;
+  appleName?: string;
+}
+
+export interface IGoogleWithSignIn {
+  idToken: string;
+  accessToken: string;
+  googleName?: string;
+}
